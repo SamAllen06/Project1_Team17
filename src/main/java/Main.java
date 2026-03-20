6b98ff61b8c0af6dd0b3f94f74d6396166e7f414
SamAllen06 <samgallen06@gmail.com>
2026-03-20 10:02:39 -0400

    Create main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.HexFormat;
+
+public class Main {
+    public static void main(String[] args){
+    }
+
+    /* How to convert string to hex:
+    HexFormat hex = HexFormat.of();
+    System.out.println(hex.toHexDigits(Byte.parseByte(string)));
+    */
+}
