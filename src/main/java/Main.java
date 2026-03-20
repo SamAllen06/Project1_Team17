41eb7a27c5e89c335b00292b5387946ff1a2dcba
SamAllen06 <samgallen06@gmail.com>
2026-03-20 10:47:40 -0400

    Start main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,22 @@
 import java.util.HexFormat;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String assemblyOutput = "";
+        String machineOutput = "";
+
+        if (input.toLowerCase().startsWith("system.out.println(\"") && input.endsWith("\");")){
+            String command = input.split("\"")[1];
+            System.out.println(command);
+        }
+
+        if (input.contains("=")){
+            //"answer=5+6;"
+            //String[] equation = input.split("=","+","-","*","/");
+        }
     }
 
     /* How to convert string to hex:
